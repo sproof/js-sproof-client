f61488b881cb4db0b6111b2a14c12bbc1454cf0b
Clemens Brunner <cle1000.cb@gmail.com>
2019-07-11 14:29:10 +0200

    Update config_receiver.js

--- example/config/config_receiver.js
+++ example/config/config_receiver.js
@@ -5,7 +5,8 @@ let config = {
     sproofCode: 'word11 word22 word33 word44 word55 word66 word77 word88 word99 word1010 word1111 word1212',
   },
   chainId: '42',
-  chain: 'ethereum'
+  chain: 'ethereum',
+  version: '0.42'
 };
 
 module.exports = config;
